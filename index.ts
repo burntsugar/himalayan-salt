50edb99001503ef9378d0dc444f241d341a1b636
burntsugar <rrakky89@gmail.com>
2020-01-31 12:25:34 +1100

    demo code

--- index.ts
+++ index.ts
@@ -2,19 +2,37 @@
  * @Author: rrr@burntsugar.rocks 
  * @Date: 2020-01-30 14:42:33 
  * @Last Modified by: rrr@burntsugar.rocks
- * @Last Modified time: 2020-01-31 10:35:54
+ * @Last Modified time: 2020-01-31 12:22:29
  */
 
  import {himalaya} from './himalaya'
-
+ console.log(`%c                         ____
+                  /^\   / -- )
+                 / | \ (____/
+                / | | \ / /
+               /_|_|_|_/ /
+                |     / /
+ __    __    __ |    / /__    __    __
+[  ]__[  ]__[  ].   / /[  ]__[  ]__[  ]
+|__            ____/ /___           __|
+   |          / .------  )         |
+   |         / /        /          |
+   |        / /        /           |
+~~~~~~~~~~~~-----------~~~~~~~~himalayan-salt Demo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`, "font-family:monospace")
+console.log();
  const pwordTest = 'testY9O/<2uWguEU';
+ console.log(`~~~~ Test password is: ${pwordTest}`);
+ console.log(`~~~~ ~~~~ Generating salt and hash...`);
  const pair: string[] = himalaya.generate256BitPaswordHash(pwordTest);
- console.log('SALT >>> ' + pair[0]);
- console.log('HASH >>> ' + pair[1]);
+ const salt = pair[0];
+ const hash = pair[1];
+ console.log(`~~~~ ~~~~ ~~~~ SALT >>>  ${salt}`);
+ console.log(`~~~~ ~~~~ ~~~~ HASH >>>  ${hash}`);
  console.log();
-
-
-// For Test
-// PASSWORD >>> testY9O/<2uWguEU
-// SALT >>> c868f62ec8e354cc9e5e15f08cd984d05407feba1d489aeed31647ce9551234b
-// HASH >>> 8e4c5f231078cee1d178560d814d88ab277c025433954c0c2bfd07b89c687cfd
+ console.log(`~~~~ rad`);
+ console.log();
+ console.log(`~~~~ ~~~~ Autheticating password: ${pwordTest}...`);
+ console.log(`~~~~ ~~~~ ~~~~ RESULT >>>  ${himalaya.authenticate(pwordTest,salt,hash)}`);
+ console.log();
+ console.log(`~~~~~~~~~~~~-----------~~~~~~~~fin~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+ 
