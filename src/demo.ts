4b1e64747fa1c4eebbac6a44aa3aa609eb4031bb
burntsugar <rrakky89@gmail.com>
2020-02-03 10:29:11 +1100

    excluding FTM

--- src/demo.ts
+++ src/demo.ts
@@ -2,41 +2,44 @@
  * @Author: rrr@burntsugar.rocks 
  * @Date: 2020-01-30 14:42:33 
  * @Last Modified by: rrr@burntsugar.rocks
- * @Last Modified time: 2020-02-01 15:15:16
+ * @Last Modified time: 2020-02-03 10:28:17
  */
 
-import { himalaya } from './himalaya'
-console.log(`%c                         ____
-                  /^\   / -- )
-                 / | \ (____/
-                / | | \ / /
-               /_|_|_|_/ /
-                |     / /
- __    __    __ |    / /__    __    __
-[  ]__[  ]__[  ].   / /[  ]__[  ]__[  ]
-|__            ____/ /___           __|
-   |          / .------  )         |
-   |         / /        /          |
-   |        / /        /           |
-~~~~~~~~~~~~-----------~~~~~~~~himalayan-salt Demo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`, "font-family:monospace")
+import { himalaya } from './himalaya';
 
-console.log();
-const passphrase1 = 'testY9O/<2uWguEU';
-console.log(`passphrase is: ${passphrase1}`);
-const pair1: string[] = himalaya.generateSHA256PassphraseHash(passphrase1);
-console.log(`SALT >>>  ${pair1[0]}`);
-console.log(`HASH >>>  ${pair1[1]}`);
-console.log(`VERIFICATION >>>  ${himalaya.verify(passphrase1, pair1[0], pair1[1])}`);
-console.log
-console.log();
-const passphrase2 = 'testY9O/<2uWguEU'; // same passphrase
-console.log(`passphrase is: ${passphrase2}`);
-const pair2: string[] = himalaya.generateSHA256PassphraseHash(passphrase2);
-console.log(`SALT >>>  ${pair2[0]}`); // unique salt,
-console.log(`HASH >>>  ${pair2[1]}`); // and hash
-console.log(`VERIFICATION >>>  ${himalaya.verify(passphrase2, pair2[0], pair2[1])}`);
-console.log
-(`~~~~~~~~~~~~-----------~~~~~~~~fin~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+
+// console.log(`%c                         ____
+//                   /^\   / -- )
+//                  / | \ (____/
+//                 / | | \ / /
+//                /_|_|_|_/ /
+//                 |     / /
+//  __    __    __ |    / /__    __    __
+// [  ]__[  ]__[  ].   / /[  ]__[  ]__[  ]
+// |__            ____/ /___           __|
+//    |          / .------  )         |
+//    |         / /        /          |
+//    |        / /        /           |
+// ~~~~~~~~~~~~-----------~~~~~~~~himalayan-salt Demo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`, "font-family:monospace")
+
+// console.log();
+// const passphrase1 = 'testY9O/<2uWguEU';
+// console.log(`passphrase is: ${passphrase1}`);
+// const pair1: string[] = himalaya.generateSHA256PassphraseHash(passphrase1);
+// console.log(`SALT >>>  ${pair1[0]}`);
+// console.log(`HASH >>>  ${pair1[1]}`);
+// console.log(`VERIFICATION >>>  ${himalaya.verify(passphrase1, pair1[0], pair1[1])}`);
+// console.log
+// console.log();
+// const passphrase2 = 'testY9O/<2uWguEU'; // same passphrase
+// console.log(`passphrase is: ${passphrase2}`);
+// const pair2: string[] = himalaya.generateSHA256PassphraseHash(passphrase2);
+// console.log(`SALT >>>  ${pair2[0]}`); // unique salt,
+// console.log(`HASH >>>  ${pair2[1]}`); // and hash
+// console.log(`VERIFICATION >>>  ${himalaya.verify(passphrase2, pair2[0], pair2[1])}`);
+// console.log
+// (`~~~~~~~~~~~~-----------~~~~~~~~fin~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
 
 
 
