168ca6323f39e78cced31f3451f465026ed974d9
burntsugar <rrakky89@gmail.com>
2020-02-03 14:40:20 +1100

    added class and interface to wrap hash gen result

---
+++ src/result.ts
@@ -0,0 +1,56 @@
+/*
+ * @Author: rrr@burntsugar.rocks 
+ * @Date: 2020-02-03 11:33:45 
+ * @Last Modified by: rrr@burntsugar.rocks
+ * @Last Modified time: 2020-02-03 14:19:17
+ */
+
+ /**
+ * @file 
+ * @description RMP
+ */
+
+/** @description Defines behaviour for a class which contains hash generation
+    results. */
+interface GeneratedResult {
+    getSalt(): string
+    getHash(): string
+    getCombined(): string
+}
+
+/**
+ * @class
+ * @implements {GeneratedResult}
+ */
+class Hashes implements GeneratedResult  {
+
+    salt: string;
+    hash: string;
+
+    /**
+     * @param {string} salt
+     * @param {string} hash
+     */
+    constructor(salt:string, hash:string){
+        this.salt = salt;
+        this.hash = hash;
+    }
+
+    /** @return {string} 64 character hash. */
+    getHash():string{
+        return this.hash;
+    }
+
+    /** @return {string} 64 character salt. */
+    getSalt():string{
+        return this.salt;
+    }
+
+    /** @return {string} where the first 64 characters contain the salt and the     remaing characters contain the hash. */
+    getCombined():string{
+        return `${this.salt}${this.hash}`;
+    }
+
+}
+
+export {GeneratedResult, Hashes};
